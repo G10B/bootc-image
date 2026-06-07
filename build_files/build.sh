ad069b4f8c669334243c42df06977664d2a1f624
G10B <96593724+G10B@users.noreply.github.com>
2026-06-08 00:18:26 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs/removes a package from fedora repos
-dnf5 remove -y waydroid 
+# dnf5 remove -y waydroid 
 
 # Use a COPR Example:
 #
